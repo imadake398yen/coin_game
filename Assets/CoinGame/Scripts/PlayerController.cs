8e3f2f8c9b2fcbf07bca2a288ba69c3af89f7d35
imadake398yen <imadake398yen@gmail.com>
2015-02-15 15:09:09 +0900

    Initial commit

---
+++ Assets/CoinGame/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	AudioSource audio;
+	public Text coinLabel;
+	int coins;
+
+	void Start () {
+		audio = GetComponent<AudioSource>();
+	}
+
+	void Update () {
+		if (coinLabel != null)
+			coinLabel.text = "Coins : " + coins.ToString();
+		else if (coinLabel == null)
+			return;
+	}
+
+	void OnControllerColliderHit (ControllerColliderHit hit) {
+		if (hit.gameObject.tag == "Item") {
+			audio.Play();
+			coins++;
+			Destroy(hit.gameObject);
+		}
+	}
+
+}
